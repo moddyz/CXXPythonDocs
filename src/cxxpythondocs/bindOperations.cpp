f00bc28943e7b2edcf185477f1ec1308af8f0a02
moddyz <moddy3d@gmail.com>
2021-01-25 22:35:20 +1300

    Formatted cpp code.

--- src/cxxpythondocs/bindOperations.cpp
+++ src/cxxpythondocs/bindOperations.cpp
@@ -7,5 +7,7 @@ using namespace cxxpythondocs;
 void BindOperations(pybind11::module& module)
 {
     module.def("AddIntegers", &AddIntegers, "Compute the sum of two integers.");
-    module.def("AddFloats", &AddFloats, "Compute the sum of two floating point numbers.");
+    module.def("AddFloats",
+               &AddFloats,
+               "Compute the sum of two floating point numbers.");
 }
